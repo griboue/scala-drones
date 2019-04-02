167b9b8c25d28163c111dadb61cffd2e41367460
namileto <gregoire.monet@gmail.com>
2019-04-02 10:19:52 +0200

    finished json structure

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,6 +7,7 @@ import play.api.libs.functional.syntax._
 
 
 object Main extends App {
+  // initialization
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
@@ -15,19 +16,31 @@ object Main extends App {
   val TOPIC="dronesinfos"
 
 
+  // Creating JSON
   val json: JsValue = Json.parse("""
   {
     "id_drone" : 1,
-    "altitude" : 40
+    "battery" : 87,
+    "altitude" : 40,
+    "temperature" : 38,
+    "speed": 20,
+    "disk_space": 20,
+    "location" : {
+      "lat" : 51.235685,
+      "long" : -1.309197
+    }
   }
   """)
+
+  // Converting JSON to String
   val jsonString = Json.stringify(json)
 
+  // Sending data
   val record = new ProducerRecord(TOPIC, "key", jsonString)
   producer.send(record)
 
   // we need to close the producer to close the connection / avoid memory usage
   producer.close()
-
-  println("Hello, World!")
+  println("End of the program!")
 }
+
